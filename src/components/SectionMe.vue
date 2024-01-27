6dad4ed84ddb14649e94e0d45971ef506ea7f81c
TimChinye <timchinye123@gmail.com>
2024-01-27 20:35:21 +0000

    hover to text width, not parent - patch

--- src/components/SectionMe.vue
+++ src/components/SectionMe.vue
@@ -29,170 +29,74 @@
 				-->
 				<table>
 					<thead>
-						<tr>
-							<th>LANGUAGES</th>
-						</tr>
+						<tr><th>LANGUAGES</th></tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td class="clickable">HTML, CSS & JS</td>
-						</tr>
-						<tr>
-							<td class="clickable">SQL</td>
-						</tr>
-						<tr>
-							<td>Python</td>
-						</tr>
-						<tr>
-							<td class="clickable">Java</td>
-						</tr>
-						<tr>
-							<td class="clickable">C#</td>
-						</tr>
-						<tr>
-							<td>PHP</td>
-						</tr>
-						<tr>
-							<td class="clickable">C++</td>
-						</tr>
-						<tr>
-							<td class="clickable">TypeScript</td>
-						</tr>
-						<tr>
-							<td class="clickable">Assembly</td>
-						</tr>
-						<tr>
-							<td>Kotlin</td>
-						</tr>
-						<tr>
-							<td>SASS & SCSS</td>
-						</tr>
+						<tr><td class="clickable">HTML, CSS & JS</td></tr>
+						<tr><td class="clickable">SQL</td></tr>
+						<tr><td>Python</td></tr>
+						<tr><td class="clickable">Java</td></tr>
+						<tr><td class="clickable">C#</td></tr>
+						<tr><td>PHP</td></tr>
+						<tr><td class="clickable">C++</td></tr>
+						<tr><td class="clickable">TypeScript</td></tr>
+						<tr><td class="clickable">Assembly</td></tr>
+						<tr><td>Kotlin</td></tr>
+						<tr><td>SASS & SCSS</td></tr>
 					</tbody>
 				</table>
 				<table>
 					<thead>
-						<tr>
-							<th>FRAMEWORKS</th>
-						</tr>
+						<tr><th>FRAMEWORKS</th></tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td class="clickable">Node.js</td>
-						</tr>
-						<tr>
-							<td class="clickable">React</td>
-						</tr>
-						<tr>
-							<td class="clickable">jQuery</td>
-						</tr>
-						<tr>
-							<td>Express</td>
-						</tr>
-						<tr>
-							<td>Angular/JS</td>
-						</tr>
-						<tr>
-							<td class="clickable">Vue.js (& Vite)</td>
-						</tr>
-						<tr>
-							<td class="clickable">.NET (C#)</td>
-						</tr>
-						<tr>
-							<td>Spring Boot</td>
-						</tr>
-						<tr>
-							<td>Bootstrap</td>
-						</tr>
-						<tr>
-							<td>TailwindCSS</td>
-						</tr>
-						<tr>
-							<td>Material UI</td>
-						</tr>
+						<tr><td class="clickable">Node.js</td></tr>
+						<tr><td class="clickable">React</td></tr>
+						<tr><td class="clickable">jQuery</td></tr>
+						<tr><td>Express</td></tr>
+						<tr><td>Angular/JS</td></tr>
+						<tr><td class="clickable">Vue.js (& Vite)</td></tr>
+						<tr><td class="clickable">.NET (C#)</td></tr>
+						<tr><td>Spring Boot</td></tr>
+						<tr><td>Bootstrap</td></tr>
+						<tr><td>TailwindCSS</td></tr>
+						<tr><td>Material UI</td></tr>
 					</tbody>
 				</table>
 				<table>
 					<thead>
-						<tr>
-							<th>TOOLS & DATABASES</th>
-						</tr>
+						<tr><th>TOOLS & DATABASES</th></tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td class="clickable">Git & Github</td>
-						</tr>
-						<tr>
-							<td>Postman</td>
-						</tr>
-						<tr>
-							<td>CLI</td>
-						</tr>
-						<tr>
-							<td class="clickable">Font Awesome</td>
-						</tr>
-						<tr>
-							<td>Chrome DevTools</td>
-						</tr>
-						<tr>
-							<td>Vercel</td>
-						</tr>
-						<tr>
-							<td class="clickable">Discord & Slack</td>
-						</tr>
-						<tr>
-							<td class="clickable">Stack Overflow</td>
-						</tr>
-						<tr>
-							<td>PostgreSQL</td>
-						</tr>
-						<tr>
-							<td>MySQL</td>
-						</tr>
-						<tr>
-							<td class="clickable">SQLite</td>
-						</tr>
+						<tr><td class="clickable">Git & Github</td></tr>
+						<tr><td>Postman</td></tr>
+						<tr><td>CLI</td></tr>
+						<tr><td class="clickable">Font Awesome</td></tr>
+						<tr><td>Chrome DevTools</td></tr>
+						<tr><td>Vercel</td></tr>
+						<tr><td class="clickable">Discord & Slack</td></tr>
+						<tr><td class="clickable">Stack Overflow</td></tr>
+						<tr><td>PostgreSQL</td></tr>
+						<tr><td>MySQL</td></tr>
+						<tr><td class="clickable">SQLite</td></tr>
 					</tbody>
 				</table>
 				<table>
 					<thead>
-						<tr>
-							<th>SOFTWARE</th>
-						</tr>
+						<tr><th>SOFTWARE</th></tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td class="clickable">Figma</td>
-						</tr>
-						<tr>
-							<td class="clickable">Photoshop</td>
-						</tr>
-						<tr>
-							<td class="clickable">Affinity Designer</td>
-						</tr>
-						<tr>
-							<td>Google Workspace</td>
-						</tr>
-						<tr>
-							<td class="clickable">Replit</td>
-						</tr>
-						<tr>
-							<td class="clickable">StackBlitz</td>
-						</tr>
-						<tr>
-							<td>Android Studio</td>
-						</tr>
-						<tr>
-							<td>Visual Studio</td>
-						</tr>
-						<tr>
-							<td>VS Code</td>
-						</tr>
-						<tr>
-							<td>IntelliJ IDEA</td>
-						</tr>
-						<tr>
-							<td>Sublime Text 3</td>
-						</tr>
+						<tr><td class="clickable">Figma</td></tr>
+						<tr><td class="clickable">Photoshop</td></tr>
+						<tr><td class="clickable">Affinity Designer</td></tr>
+						<tr><td>Google Workspace</td></tr>
+						<tr><td class="clickable">Replit</td></tr>
+						<tr><td class="clickable">StackBlitz</td></tr>
+						<tr><td>Android Studio</td></tr>
+						<tr><td>Visual Studio</td></tr>
+						<tr><td>VS Code</td></tr>
+						<tr><td>IntelliJ IDEA</td></tr>
+						<tr><td>Sublime Text 3</td></tr>
 					</tbody>
 				</table>
 			</main>
@@ -264,14 +168,18 @@
 	}
 
 	section > #skills > #lists td {
+		float: left;
 		font-size: 0.75rem;
 		white-space: nowrap;
 		transition: 0.25s;
 	}
 
+	section > #skills > #lists tr:has(td.clickable) {
+		cursor: pointer;
+	}
+
 	section > #skills > #lists td.clickable {
 		position: relative;
-		cursor: pointer;
 	}
 
 	section > #skills > #lists td.clickable:after {
