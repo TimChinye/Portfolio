c1643b854d93cf5e20d8089813813c2c5ae299e4
Tim <timchinye123@gmail.com>
2026-04-05 21:38:27 +0100

    Fixed favicon

--- src/app/(portfolio)/layout.tsx
+++ src/app/(portfolio)/layout.tsx
@@ -1,16 +1,22 @@
 "use client";
 
+import { useParams } from "next/navigation";
 import { ThemeProvider } from "@/components/ui/ThemeProvider";
 import { ProgressBar } from '@/components/features/ProgressBar';
 import { Navbar } from '@/components/shared/Navbar';
+import { Favicon } from '@/components/shared/Favicon';
 
 export default function PortfolioLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const params = useParams();
+  const variant = params.variant as 'tim' | 'tiger';
+
   return (
     <>
+      <Favicon variant={variant || 'tim'} />
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <ProgressBar />
         <Navbar />
